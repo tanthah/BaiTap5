b9d89ef4c3cd09c9ba1ee76df3a2150dbfbc1bcb
tanthah <165071150+tanthah@users.noreply.github.com>
2025-11-28 23:08:24 +0700

    Delete Fontend directory

--- Fontend/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './redux/authSlice';
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-});
